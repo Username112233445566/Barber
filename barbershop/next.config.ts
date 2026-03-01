cf574cbeb2c726936228eb9570092d0544c950a6
Username112233445566 <casenov2101@gmail.com>
2026-03-01 18:45:43 +0600

    fix: redirect

--- barbershop/next.config.ts
+++ barbershop/next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/site",
+        permanent: false,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
